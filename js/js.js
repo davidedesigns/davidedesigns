24032d6f198615e71b4771774eb1845848466c39
Davide Bianchi <davidebianchi@Davides-MBP-2.fios-router.home>
2022-01-06 14:11:52 -0500

    initial commit 3

---
+++ js/js.js
@@ -0,0 +1,167 @@
+const monographBlock = document.getElementById("monographBlock")
+const monographImage = document.getElementById("monograph")
+const monographText = document.getElementById("monographText")
+monographBlock.addEventListener('mouseover', (event) =>{
+  monographImage.classList.add('blur')
+  monographText.classList.remove('hidden')
+})
+monographBlock.addEventListener('mouseleave', (event) =>{
+  monographImage.classList.remove('blur')
+  monographText.classList.add('hidden')
+})
+
+const wikipamphletBlock = document.getElementById("wikipamphletBlock")
+const wikipamphletImage = document.getElementById("wikipamphlet")
+const wikipamphletText = document.getElementById("wikipamphletText")
+wikipamphletBlock.addEventListener('mouseover', (event) =>{
+  wikipamphletImage.classList.add('blur')
+  wikipamphletText.classList.remove('hidden')
+})
+wikipamphletBlock.addEventListener('mouseleave', (event) =>{
+  wikipamphletImage.classList.remove('blur')
+  wikipamphletText.classList.add('hidden')
+})
+
+const thoughtsonthesisBlock = document.getElementById("thoughtsonthesisBlock")
+const thoughtsonthesisImage = document.getElementById("thoughtsonthesis")
+const thoughtsonthesisText = document.getElementById("thoughtsonthesisText")
+thoughtsonthesisBlock.addEventListener('mouseover', (event) =>{
+  thoughtsonthesisImage.classList.add('blur')
+  thoughtsonthesisText.classList.remove('hidden')
+})
+thoughtsonthesisBlock.addEventListener('mouseleave', (event) =>{
+  thoughtsonthesisImage.classList.remove('blur')
+  thoughtsonthesisText.classList.add('hidden')
+})
+
+const impactempathyBlock = document.getElementById("impactempathyBlock")
+const impactempathyImage = document.getElementById("impactempathy")
+const impactempathyText = document.getElementById("impactempathyText")
+impactempathyBlock.addEventListener('mouseover', (event) =>{
+  impactempathyImage.classList.add('blur')
+  impactempathyText.classList.remove('hidden')
+})
+impactempathyBlock.addEventListener('mouseleave', (event) =>{
+  impactempathyImage.classList.remove('blur')
+  impactempathyText.classList.add('hidden')
+})
+
+const interviewBlock = document.getElementById("interviewBlock")
+const interviewImage = document.getElementById("interview")
+const interviewText = document.getElementById("interviewText")
+interviewBlock.addEventListener('mouseover', (event) =>{
+  interviewImage.classList.add('blur')
+  interviewText.classList.remove('hidden')
+})
+interviewBlock.addEventListener('mouseleave', (event) =>{
+  interviewImage.classList.remove('blur')
+  interviewText.classList.add('hidden')
+})
+
+const typespecimenBlock = document.getElementById("typespecimenBlock")
+const typespecimenImage = document.getElementById("typespecimen")
+const typespecimenText = document.getElementById("typespecimenText")
+typespecimenBlock.addEventListener('mouseover', (event) =>{
+  typespecimenImage.classList.add('blur')
+  typespecimenText.classList.remove('hidden')
+})
+typespecimenBlock.addEventListener('mouseleave', (event) =>{
+  typespecimenImage.classList.remove('blur')
+  typespecimenText.classList.add('hidden')
+})
+
+const catalogofinfluencesBlock = document.getElementById("catalogofinfluencesBlock")
+const catalogofinfluencesImage = document.getElementById("catalogofinfluences")
+const catalogofinfluencesText = document.getElementById("catalogofinfluencesText")
+catalogofinfluencesBlock.addEventListener('mouseover', (event) =>{
+  catalogofinfluencesImage.classList.add('blur')
+  catalogofinfluencesText.classList.remove('hidden')
+})
+catalogofinfluencesBlock.addEventListener('mouseleave', (event) =>{
+  catalogofinfluencesImage.classList.remove('blur')
+  catalogofinfluencesText.classList.add('hidden')
+})
+
+const fitappBlock = document.getElementById("fitappBlock")
+const fitappImage = document.getElementById("fitapp")
+const fitappText = document.getElementById("fitappText")
+fitappBlock.addEventListener('mouseover', (event) =>{
+  fitappImage.classList.add('blur')
+  fitappText.classList.remove('hidden')
+})
+fitappBlock.addEventListener('mouseleave', (event) =>{
+  fitappImage.classList.remove('blur')
+  fitappText.classList.add('hidden')
+})
+
+const tuscBlock = document.getElementById("tuscBlock")
+const tuscImage = document.getElementById("tusc")
+const tuscText = document.getElementById("tuscText")
+tuscBlock.addEventListener('mouseover', (event) =>{
+  tuscImage.classList.add('blur')
+  tuscText.classList.remove('hidden')
+})
+tuscBlock.addEventListener('mouseleave', (event) =>{
+  tuscImage.classList.remove('blur')
+  tuscText.classList.add('hidden')
+})
+
+const sfmomarebrandBlock = document.getElementById("sfmomarebrandBlock")
+const sfmomarebrandImage = document.getElementById("sfmomarebrand")
+const sfmomarebrandText = document.getElementById("sfmomarebrandText")
+sfmomarebrandBlock.addEventListener('mouseover', (event) =>{
+  sfmomarebrandImage.classList.add('blur')
+  sfmomarebrandText.classList.remove('hidden')
+})
+sfmomarebrandBlock.addEventListener('mouseleave', (event) =>{
+  sfmomarebrandImage.classList.remove('blur')
+  sfmomarebrandText.classList.add('hidden')
+})
+
+const digitalartBlock = document.getElementById("digitalartBlock")
+const digitalartImage = document.getElementById("digitalart")
+const digitalartText = document.getElementById("digitalartText")
+digitalartBlock.addEventListener('mouseover', (event) =>{
+  digitalartImage.classList.add('blur')
+  digitalartText.classList.remove('hidden')
+})
+digitalartBlock.addEventListener('mouseleave', (event) =>{
+  digitalartImage.classList.remove('blur')
+  digitalartText.classList.add('hidden')
+})
+
+const shoesBlock = document.getElementById("shoesBlock")
+const shoesImage = document.getElementById("shoes")
+const shoesText = document.getElementById("shoesText")
+shoesBlock.addEventListener('mouseover', (event) =>{
+  shoesImage.classList.add('blur')
+  shoesText.classList.remove('hidden')
+})
+shoesBlock.addEventListener('mouseleave', (event) =>{
+  shoesImage.classList.remove('blur')
+  shoesText.classList.add('hidden')
+})
+
+const voteBlock = document.getElementById("voteBlock")
+const voteImage = document.getElementById("vote")
+const voteText = document.getElementById("voteText")
+voteBlock.addEventListener('mouseover', (event) =>{
+  voteImage.classList.add('blur')
+  voteText.classList.remove('hidden')
+})
+voteBlock.addEventListener('mouseleave', (event) =>{
+  voteImage.classList.remove('blur')
+  voteText.classList.add('hidden')
+})
+
+const wavestriptychBlock = document.getElementById("wavestriptychBlock")
+const wavestriptychImage = document.getElementById("wavestriptych")
+const wavestriptychText = document.getElementById("wavestriptychText")
+wavestriptychBlock.addEventListener('mouseover', (event) =>{
+  wavestriptychImage.classList.add('blur')
+  wavestriptychText.classList.remove('hidden')
+})
+wavestriptychBlock.addEventListener('mouseleave', (event) =>{
+  wavestriptychImage.classList.remove('blur')
+  wavestriptychText.classList.add('hidden')
+})
